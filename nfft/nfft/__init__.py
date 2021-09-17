e2404fb99871bb81f9fac9829965e9a8ed3cc60e
Gavin Weir <gawe@ipp-hgw.mpg.de>
2021-09-17 16:35:04 +0200

    removing the submodule stuff from master branch after merge from development

--- nfft/nfft/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Non-equispaced Fast Fourier Transforms"""
-
-from __future__ import absolute_import, division, print_function
-from .version import __version__
-from .core import nfft, ndft, nfft_adjoint, ndft_adjoint
--- wavelets/swan-0.7.1/swan_modules/gui/__init__.py
+++
@@ -1,2 +0,0 @@
-from . import swancmap
-swanrgb = swancmap.swanrgb
